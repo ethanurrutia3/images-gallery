93c02c315f5eb6e2ae8161f12f2a924a1ebd3543
Ethan Urrutia <ethandavid3@icloud.com>
2024-06-18 16:16:57 -0500

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 function App() {
   return (
     <div>
-      <h1> Images Galery </h1>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
